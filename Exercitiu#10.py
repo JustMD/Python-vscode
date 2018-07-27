91a33495fc67aea299785e0bf4f6fe3c9ae376e0
Madalin <just.matei.madalin@gmail.com>
2018-07-27 22:14:57 +0300

    competed exercise 10

--- Exercitiu#10.py
+++ Exercitiu#10.py
@@ -1,2 +1,22 @@
+import random
+
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+list1 = random.sample(range(1, 12), 8)
+list2 = random.sample(range(1, 13), 6)
+
+print(list1)
+print(list2)
+
+# list3 = []
+
+# for list1[i] in list1 :
+#     if list1[i] in list2 :
+#         if list1[i] not in list3 :
+#             list3.append(list1[i])
+
+# print(list3)
+
+list4 = [i for i in set(list1) if i in list2]
+result = print([i for i in list4 if list4.count(i) == 1])
